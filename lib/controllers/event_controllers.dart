54dffe3090f481a729cea0ea6f871162d87eb728
Khalil <khalilhenane@hotmail.com>
2025-01-01 23:09:03 +0100

    Made some Card skechtes to pick a bit of a structure

--- lib/controllers/event_controllers.dart
+++ lib/controllers/event_controllers.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
-
 import 'package:supabase/supabase.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-
-class  EventControllers {
-final supabase = Supabase.instance.client;
+class EventControllers {
+  final supabase = Supabase.instance.client;
   // final SupabaseClient supaBase = Supabase.instance.client;
 
+  retrieveEvents() async {
+    final response = await supabase.from('events').select().select();
 
-
-
-  retrieveEvents() {
-    
-    // return await supabase.from('events').select().execute();
+    if (response == null) {
+      print('Error retrieving events: ${response}');
+      return [];
+    }
+    print('Events retrieved successfully ${response}');
+    return response;
   }
 }
